34156a14654e090695da2ad9f557f87f808fe10f
Milton Espinoza <milton@senorcoders.com>
2018-09-24 12:45:45 -0600

    adding service for user contacts

---
+++ src/app/services/contact.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+  doctorID: string;
+
+  constructor( private http: HttpClient, private auth: AuthService ) { 
+    this.doctorID = auth.doctorID;
+  }
+
+  saveContact( contact: any ) {
+    return this.http.post( environment.base_api + '/contact', contact )
+  }
+
+  deleteContact ( contact_id: string ) {
+    return this.http.delete( environment.base_api + '/contact/' + contact_id )
+  }
+}
--- src/app/services/doctor.service.ts
+++ src/app/services/doctor.service.ts
@@ -7,6 +7,8 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class DoctorService {
   doctorID: string;
   constructor(private http: HttpClient, private auth: AuthService) {
@@ -17,9 +19,5 @@ export class DoctorService {
     return this.http.get(`${environment.base_api}/doctor/${this.doctorID}`);
   }
 
-  /*uploadAvatar() {
-    const fd = new FormData();
-    fd.append('image', this.selectedFile)
-    this.http.post('http://138.68.19.227:7002/services/5ba00d8bde1f890136510634/image', )
-  }*/
+
 }
